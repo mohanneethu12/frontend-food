8f4bffbc48fac78936ffc9933a28f0024991ef24
mohanneethu12 <mohanneethu1306@gmail.com>
2024-11-13 16:42:51 +0530

    Add files via upload

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className='footer-content-left'>
+                    <img src={assets.logo} alt="" />
+                    <p>Discover delicious meals from your favorite local restaurants with Tomato. Order with ease and enjoy fast delivery straight to your door. Join our community of food lovers and explore a wide variety of cuisines, all from the comfort of your home.</p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+
+                <div className='footer-content-center'>
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div className='footer-content-right'>
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+1-212-456-7890</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+
+            </div>
+            <hr />
+            <p className='footer-copyright'>Copyright 2024 &copy; Tomato.com - All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
